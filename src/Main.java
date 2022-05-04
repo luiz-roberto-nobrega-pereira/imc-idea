ebe41d71787bbc037fda09d469419f85e928ce8d
luiz-roberto-nobrega-pereira <104457234+luiz-roberto-nobrega-pereira@users.noreply.github.com>
2022-05-04 16:21:45 -0300

    função de cadastrar nome e senha.

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,23 @@ public class Main {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Informe o seu nome: ");
+        String nome = sc.nextLine();
+        System.out.println("Cadastre a sua senha: ");
+        int senha = sc.nextInt();
+        System.out.println("Informe a sua senh cadastrada: ");
+        int senhaCadastrada = sc.nextInt();
+        while(senha != senhaCadastrada){
+            System.out.println("Informe a sua senh cadastrada novamente: ");
+            senhaCadastrada = sc.nextInt();
+        }
+
         System.out.println("Informe o seu peso: ");
         double peso = sc.nextDouble();
         System.out.println("Informe a sua altura: ");
         double altura = sc.nextDouble();
         double imc = peso/(altura*altura);
         System.out.printf("IMC: %.2f",imc);
-
-
 
         sc.close();
 
